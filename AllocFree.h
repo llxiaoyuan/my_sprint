fde052611a681058042176a2b1ffcc0db08930dd
Chase <36320938+llxiaoyuan@users.noreply.github.com>
2023-09-23 18:48:03 +0800

    update

---
+++ AllocFree.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	typedef void* (*__Alloc)(size_t BufferSize);
+	typedef void (*__Free)(void* Buffer);
+	typedef void* (*__ReAlloc)(void* Buffer, size_t NewBufferSize);
+	typedef void* (*__ReAllocExpand)(void* OldBuffer, size_t NewBufferSize, size_t OldBufferSize);
+
+	void* Alloc(size_t BufferSize);
+	void Free(void* Buffer);
+	void* ReAlloc(void* OldBuffer, size_t NewBufferSize);
+	void* ReAllocExpand(void* OldBuffer, size_t NewBufferSize, size_t OldBufferSize);
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
